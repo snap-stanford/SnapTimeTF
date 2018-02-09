47dd508a2ac38218baaa34052b7277169d910e4e
Michael Chen <mc.michaelchen.us@gmail.com>
2018-02-09 14:37:08 -0800

    Update example to work with validation

--- example.py
+++ example.py
@@ -4,10 +4,10 @@ from predictor import *
 
 reader = Reader()
 shape = reader.get_shapes()
-dense_values, frame_values, num_sensors, num_timesteps = reader.read(32, shape=shape)
-predictions = tf.layers.dense(dense_values)
+val = tf.placeholder_with_default(False, [])
+dense_values, frame_values, num_sensors, num_timesteps = reader.read(32, shape=shape, val=val)
+predictions = tf.layers.dense(dense_values, shape[0])
 
 with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
     sess.run(predictions)
-
-
